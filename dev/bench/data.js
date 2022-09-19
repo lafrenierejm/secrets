c77066d8b1b3e16eb5c7eedc32fb157a04c793a5
github-action-benchmark <github@users.noreply.github.com>
2022-09-19 04:56:16 +0000

    add Benchmark (cargo) benchmark result for 58379a07eaba18e363faa677c882fb451c418d6a

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1663563376286,
+  "repoUrl": "https://github.com/lafrenierejm/ripsecrets",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Joseph LaFreniere",
+            "username": "lafrenierejm",
+            "email": "joseph@lafreniere.xyz"
+          },
+          "committer": {
+            "name": "Joseph LaFreniere",
+            "username": "lafrenierejm",
+            "email": "joseph@lafreniere.xyz"
+          },
+          "id": "58379a07eaba18e363faa677c882fb451c418d6a",
+          "message": "Run criterion with bench-compatible output",
+          "timestamp": "2022-09-19T04:44:52Z",
+          "url": "https://github.com/lafrenierejm/ripsecrets/commit/58379a07eaba18e363faa677c882fb451c418d6a"
+        },
+        "date": 1663563375246,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Find secrets in getsentry/sentry/find_secrets function",
+            "value": 8639225428,
+            "range": "± 1451377931",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
