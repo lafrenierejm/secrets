38ffb4e8ca1056dfae200bfc92aa382c5fd2b3cd
github-action-benchmark <github@users.noreply.github.com>
2022-09-19 05:31:01 +0000

    add Benchmark (cargo) benchmark result for 24f2b6b131e325513c33d23244100541212ab874

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663563376286,
+  "lastUpdate": 1663565461497,
   "repoUrl": "https://github.com/lafrenierejm/ripsecrets",
   "entries": {
     "Benchmark": [
@@ -27,6 +27,34 @@ window.BENCHMARK_DATA = {
             "name": "Find secrets in getsentry/sentry/find_secrets function",
             "value": 8639225428,
             "range": "± 1451377931",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Joseph LaFreniere",
+            "username": "lafrenierejm",
+            "email": "joseph@lafreniere.xyz"
+          },
+          "committer": {
+            "name": "Joseph LaFreniere",
+            "username": "lafrenierejm",
+            "email": "joseph@lafreniere.xyz"
+          },
+          "id": "24f2b6b131e325513c33d23244100541212ab874",
+          "message": "Delete Markdown files to avoid Jekyll errors",
+          "timestamp": "2022-09-19T05:22:40Z",
+          "url": "https://github.com/lafrenierejm/ripsecrets/commit/24f2b6b131e325513c33d23244100541212ab874"
+        },
+        "date": 1663565459330,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Find secrets in getsentry/sentry/find_secrets function",
+            "value": 10126913113,
+            "range": "± 1635728423",
             "unit": "ns/iter"
           }
         ]
