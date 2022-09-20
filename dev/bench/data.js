2eea1409d962443d2a8eef450cc2afcd05ccd83e
github-action-benchmark <github@users.noreply.github.com>
2022-09-20 00:24:24 +0000

    add Benchmark (cargo) benchmark result for c18ca76357bf40c58cad3ad52829f8baac2d3fc8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663565461497,
+  "lastUpdate": 1663633464900,
   "repoUrl": "https://github.com/lafrenierejm/ripsecrets",
   "entries": {
     "Benchmark": [
@@ -55,6 +55,36 @@ window.BENCHMARK_DATA = {
             "name": "Find secrets in getsentry/sentry/find_secrets function",
             "value": 10126913113,
             "range": "± 1635728423",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joseph@lafreniere.xyz",
+            "name": "Joseph LaFreniere",
+            "username": "lafrenierejm"
+          },
+          "committer": {
+            "email": "joseph@lafreniere.xyz",
+            "name": "Joseph LaFreniere",
+            "username": "lafrenierejm"
+          },
+          "distinct": true,
+          "id": "c18ca76357bf40c58cad3ad52829f8baac2d3fc8",
+          "message": "Create a GitHub Actions workflow job for benchmarking",
+          "timestamp": "2022-09-19T19:17:14-05:00",
+          "tree_id": "3277761c2e7566aa933829005a557081947a84a3",
+          "url": "https://github.com/lafrenierejm/ripsecrets/commit/c18ca76357bf40c58cad3ad52829f8baac2d3fc8"
+        },
+        "date": 1663633464126,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Find secrets in getsentry/sentry/find_secrets function",
+            "value": 7293060731,
+            "range": "± 718965945",
             "unit": "ns/iter"
           }
         ]
