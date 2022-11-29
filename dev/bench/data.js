f4876a9e117748287bdc8df5e292ea4837b65b36
github-action-benchmark <github@users.noreply.github.com>
2022-11-29 21:37:38 +0000

    add Benchmark (cargo) benchmark result for 30a3c80f4ecde56780e6e50216332601979d3775

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669757849139,
+  "lastUpdate": 1669757858680,
   "repoUrl": "https://github.com/lafrenierejm/ripsecrets",
   "entries": {
     "Benchmark": [
@@ -115,6 +115,36 @@ window.BENCHMARK_DATA = {
             "name": "Find secrets in getsentry/sentry/find_secrets function",
             "value": 7612351239,
             "range": "± 442403783",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joseph@lafreniere.xyz",
+            "name": "Joseph M LaFreniere",
+            "username": "lafrenierejm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30a3c80f4ecde56780e6e50216332601979d3775",
+          "message": "Add pre-commit checks (#45)\n\n* Configure pre-commit hooks as part of Nix shell\r\n\r\nThe pre-commit hooks added are\r\n- nixfmt\r\n- rustfmt\r\n- cargo check\r\n- clippy\r\n\r\n* Autoformat Nix files using `nixfmt`\r\n\r\n* Run pre-commit via Nix as part of CI workflow\r\n\r\n* Gitignore `/result`\r\n\r\n* Autoformat Rust source files using `rustfmt`\r\n\r\n* Skip build as part of `nix flake check`",
+          "timestamp": "2022-11-28T20:33:50-08:00",
+          "tree_id": "1103fbb86d276a351bc527246f94194ebae2db2d",
+          "url": "https://github.com/lafrenierejm/ripsecrets/commit/30a3c80f4ecde56780e6e50216332601979d3775"
+        },
+        "date": 1669757857574,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Find secrets in getsentry/sentry/find_secrets function",
+            "value": 10388274681,
+            "range": "± 842500625",
             "unit": "ns/iter"
           }
         ]
